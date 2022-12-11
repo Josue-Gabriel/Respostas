19b16352895115ca4ddeb9cef070adee34bf27c2
Josue-Gabriel <103777382+Josue-Gabriel@users.noreply.github.com>
2022-12-11 18:22:43 -0300

    Commit inicial

---
+++ quarta_questão.py
@@ -0,0 +1,19 @@
+#SP – R$67.836,43
+#RJ – R$36.678,66
+#MG – R$29.229,88
+#ES – R$27.165,48
+#Outros – R$19.849,53
+
+#Calculo da porcentagem mensal.
+sp = int(67.83643 * 30 / 100)
+rj = int(36.67866 * 30 / 100)
+mg = int(29.22988 * 30 / 100)
+es = int(27.16548 * 30 / 100)
+outros = int(19.84953 * 30 / 100)
+
+#Amostra do resultado.
+print("O porcentual de São Paulo é: ", sp, "%")
+print("O porcentual de São Paulo é: ", rj, "%")
+print("O porcentual de São Paulo é: ", mg, "%")
+print("O porcentual de São Paulo é: ", es, "%")
+print("O porcentual de São Paulo é: ", outros, "%")
