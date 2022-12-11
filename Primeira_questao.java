c09c62d7136082ce19c4b7f0305f9e819591d57a
Josue-Gabriel <103777382+Josue-Gabriel@users.noreply.github.com>
2022-12-11 11:58:01 -0300

    Primeiro commit

---
+++ Primeira_questao.java
@@ -0,0 +1,16 @@
+package com.mycompany.primeira_questao;
+
+
+public class Primeira_questao {
+
+    public static void main(String[] args) {
+        int indice = 13;
+        int soma = 0;
+        int k = 0;
+        if(k < indice){
+            k = k + 1;
+            soma = soma + k;
+            System.out.println(soma);
+        }
+    }
+}
