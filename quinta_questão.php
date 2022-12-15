ded947eb4912636578abc9b019c6aabf2ad93ffb
Josue-Gabriel <103777382+Josue-Gabriel@users.noreply.github.com>
2022-12-15 07:23:02 -0300

    Fase final do código
    -New
     *Campo para texto.
     *Corpo HTML.

--- quinta_questão.php
+++ quinta_questão.php
@@ -1,3 +1,19 @@
-<?php
-  echo strrev("Ola, mundo");
-?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <style>
+    </style>
+  </head>
+  <body>
+    <div class="invertido">
+      <form class="" method="post">
+        <input type="text" name="inversao">
+      </form>
+    </div>
+    <?php
+      $texto = $_POST['inversao'];
+      echo strrev($texto);
+    ?>
+  </body>
+</html>
